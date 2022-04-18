d294b59c21d33f9e7a7eac6e510c04a91ede5101
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-04-18 16:27:45 -0500

    Agregando funcionalidad: Pantalla Completa

--- app/src/main/java/com/example/duelodevaqueros/MainActivity.java
+++ app/src/main/java/com/example/duelodevaqueros/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.duelodevaqueros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Método que permite ejecutar a la app en pantalla completa usando el modo Inmersivo.
+     * Con un gesto táctil se mostrará la barra de notificaciones y los botones del sistema
+     * al usuario, los cuales se ocultan para permitir mostrar la aplicación en pantalla completa.
+     * @param hasFocus indica si la pantalla está siendo enfocada.
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
     }
 }
