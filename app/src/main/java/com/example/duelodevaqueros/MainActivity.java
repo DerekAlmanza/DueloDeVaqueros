f3af58581455ee198383a6e2ea53d13f07b6ae24
Helen Salazar <helen2000@ciencias.unam.mx>
2022-04-20 23:15:15 -0500

    Pantalla Encendida

--- app/src/main/java/com/example/duelodevaqueros/MainActivity.java
+++ app/src/main/java/com/example/duelodevaqueros/MainActivity.java
@@ -11,4 +11,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+        }
+    }
+
+    public void finalCountdown(View startButton) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void fire(View gun) {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
 }
